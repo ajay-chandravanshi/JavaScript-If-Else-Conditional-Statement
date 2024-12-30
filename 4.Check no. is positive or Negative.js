990331f3bfec3aad15bd11805817d0d863392541
ajay-chandravanshi <ajaystudy38@gmail.com>
2024-12-31 00:44:19 +0530

    new code

---
+++ 4.Check no. is positive or Negative.js
@@ -0,0 +1,8 @@
+let number=prompt("Enter the Number");
+if(number>0)
+{
+    alert("Entered Number is Positive");
+}
+else{
+    alert("Entered Number is Negative");
+}
---
+++ 5.Check Fail or pass in the paper.js
@@ -0,0 +1,8 @@
+let score=prompt("Enter your paper marks");
+if(score>=50)
+{
+  alert("You are pass");
+}
+else{
+    alert("You are fail");
+}
